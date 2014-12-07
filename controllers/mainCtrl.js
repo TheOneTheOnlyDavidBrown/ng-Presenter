4690ea2c6501c3e3b3711b05fddeca13f33d89ad
David Brown <david@davidbrownucf.com>
2014-12-07 13:54:30 -0500

    syntax fix

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -1,5 +1,5 @@
 myApp.controller('mainCtrl',['$scope','localStorageService',function($scope, localStorageService){
   $scope.getRandomSpan = function(){
     return Math.floor((Math.random()*999999999999));
-  }
+  };
 }]);
